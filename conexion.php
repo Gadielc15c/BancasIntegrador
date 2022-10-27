16b40d48b8bf6bd4e86493d5d1c71b4a1ed0d454
Gadiel <Gadielcascante152001@gmail.com>
2022-10-27 11:59:33 -0400

    Se agrego Polimorfismo Con el NAV, Se Cambiaron extenciones y se cambio el index momentaneamente

--- conexion.php
+++ conexion.php
@@ -1,25 +1,31 @@
 <?php 
 	// Parametros a configurar para la conexion de la base de datos 
+	
+function conectar(){
 	$host = "us-cdbr-east-06.cleardb.net";    // sera el HOST v de nuestra BD 
 	$basededatos = "heroku_607c4ead21a11f6";    // sera el Nombre de nuestra BD 
-	$usuariodb = " b04805fc3016f3";    // sera el USER de nuestra BD 
-	$clavedb = "3306";    // sera el PASS de nuestra BD 
-	
+	$usuariodb = "b04805fc3016f3";    // sera el USER de nuestra BD 
+	$clavedb = "ca4df059";    // sera el PASS de nuestra BD 
+	$port ="3306";
     
     $tabla_db1 = "empleados"; 	   //Campo no necesario solo utilizo para pruebas  GADIEL CASCANTE 
 	
 
 	
 	
-	$conexion = new mysqli($host,$usuariodb,$clavedb,$basededatos);
+	$conexion = new mysqli($host,$usuariodb,$clavedb,$basededatos,$port );
+	return $conexion;
 
 
-	if ($conexion->connect_errno) {
+	if  ($conexion->connect_errno) {
 	    echo "Nuestro sitio experimenta fallos....";
-
+		die('Connect Error (' . $dblink->connect_errno . ') '. $dblink->connect_error);
               #TODO agregar formulario de error
               
 	    exit();
 	}
-
+}
 ?>
+
+
+
---
+++ nav.php
@@ -0,0 +1,37 @@
+
+<div id="container">
+    <div class="top">
+      <div class="nav">
+        <div class="logoDiv">
+          <a class="navbar-logo" href="#">
+            <img src="img/Logo.png" />
+          </a>
+        </div>
+        <div class="opciones">
+          <a href="#" class="NavItem NavButton">
+            <ion-icon name="settings"></ion-icon>
+            </ion-icon>
+          </a>
+          <a href="#" class="NavItem NavButton">
+            <ion-icon name="person"></ion-icon> CUENTA
+          </a>
+          <a href="#" class="NavItem NavButton">
+            <ion-icon name="wallet"></ion-icon> PAGOS
+          </a>
+          <a href="#" class="NavItem NavButton">
+            <ion-icon name="wallet"></ion-icon> Mantenimientos
+          </a>
+          <a href="#" class="NavItem NavButton">
+            <ion-icon name="book"></ion-icon> JUGADAS
+          </a>
+          <a href="#" class="NavItem NavButton">
+            <ion-icon name="apps"></ion-icon> DASHBOARD
+          </a>
+          <a href="#" class="NavItem NavButton">
+            <ion-icon name="home"></ion-icon> INICIO
+          </a>
+        </div>
+      </div>
+    </div>
+
+  </div>
