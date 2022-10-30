81776ff0c183461490c909a6b269371b8f14afa8
Cristopher9807 <114347569+Cristopher9807@users.noreply.github.com>
2022-10-30 01:08:57 -0400

    header

--- frontend/mantenimientoTicket.php
+++ frontend/mantenimientoTicket.php
@@ -18,9 +18,9 @@
 
 <body>
     
-<div class="container mt-5">
-   <div class="row">
-     <div class="col-md-6">
+<div class="container2">
+   <div class="container">
+    
          
      <form action="">
                 <input type="text" class="form-control mb-3" name="idticket" placeholder="Id Ticket">
@@ -34,7 +34,7 @@
 
 
 
-     </div>
+   
 
 
 
