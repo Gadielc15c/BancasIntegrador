42592c69556dd18127e3c832e33871cbda75035d
Gadiel <Gadielcascante152001@gmail.com>
2022-11-01 00:49:44 -0400

    Seteo de Permismos de Paginas

--- cliente/dashboardCliente.php
+++ cliente/dashboardCliente.php
@@ -1,4 +1,18 @@
-<!DOCTYPE html>
+<?php
+
+session_start();
+if(!isset($_SESSION['nivel'])){
+
+header('location:  ../index.php');
+    
+}else {
+    if($_SESSION['nivel']!=4){
+
+header('location:  ../index.php');
+
+    }
+}
+?><!DOCTYPE html>
 <html lang="en">
 
 <head>
