efee3f8a436d1b440e5d6212f88320a911922b28
MarielaPM02 <114320802+MarielaPM02@users.noreply.github.com>
2022-11-14 00:54:06 -0400

    prueba

---
+++ backend/prueba.php
@@ -0,0 +1,23 @@
+<?php
+
+$conexion= conexion();
+$consulta='';
+
+function select_historial_jugadas(){
+    global $conexion, $consulta;
+
+    $sql ="SELECT J.idjugadas, J.jugnumeros,
+                    T.nombre,
+                    L.nombre,
+                    T.idticket,T.monto, M.moneda, T.fecha, T.estado, TER.nomusuarios, T.codigobarra
+                    FROM jugadas J
+                    INNER JOIN tipojugadas T ON J.idtipojugada_fk = T.idtipojugadas
+                    INNER JOIN loterias L ON J.idloteria_fk  = L.idloteria
+                    INNER JOIN tickets T ON J.idticket_fk = T.idticket
+                    INNER JOIN monedas M ON T.monedas_fk = M.idmonedas
+                    INNER JOIN terceros TER ON T.idterceros_fk = TER.idterceros";
+
+                    return $conexion->query($sql);
+
+}
+?>
