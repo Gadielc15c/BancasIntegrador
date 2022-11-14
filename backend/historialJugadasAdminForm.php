4f1b8e811e005a5464592a44edc709d9fee7d5e2
MarielaPM02 <114320802+MarielaPM02@users.noreply.github.com>
2022-11-14 15:39:46 -0400

    modificacion en referencia de la tabla ticket

--- backend/historialJugadasAdminForm.php
+++ backend/historialJugadasAdminForm.php
@@ -4,13 +4,13 @@ $con = conectar();
 $sql= "SELECT J.idjugadas, J.jugnumeros,
 T.nombre,
 L.nombre,
-T.idticket,T.monto, M.moneda, T.fecha, T.estado, TER.nomusuarios, T.codigobarra
+TI.idticket,TI.monto, M.moneda, TI.fecha, TI.estado, TER.nomusuarios, TI.codigobarra
 FROM jugadas J
 INNER JOIN tipojugadas T ON J.idtipojugada_fk = T.idtipojugadas
 INNER JOIN loterias L ON J.idloteria_fk  = L.idloteria
-INNER JOIN tickets T ON J.idticket_fk = T.idticket
-INNER JOIN monedas M ON T.monedas_fk = M.idmonedas
-INNER JOIN terceros TER ON T.idterceros_fk = TER.idterceros";
+INNER JOIN tickets TI ON J.idticket_fk = TI.idticket
+INNER JOIN monedas M ON TI.monedas_fk = M.idmonedas
+INNER JOIN terceros TER ON TI.idterceros_fk = TER.idterceros";
 $query=mysqli_query($con,$sql);
 
 
