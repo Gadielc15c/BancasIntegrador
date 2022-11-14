3505ec25f50ec411ad8f7f431b1def08fdaca65c
MarielaPM02 <114320802+MarielaPM02@users.noreply.github.com>
2022-11-14 00:45:14 -0400

    arreglos rutas

--- backend/prueba.php
+++ backend/prueba.php
@@ -12,11 +12,13 @@ function select_historial_jugadas(){
     $sql ="SELECT J.idjugadas, J.jugnumeros,
                     T.nombre,
                     L.nombre,
-                    T.idticket,T.monto, T.moneda_fk, T.fecha, T.estado, T.idterceros_fk, T.codigobarra
+                    T.idticket,T.monto, M.moneda, T.fecha, T.estado, TER.nomusuarios, T.codigobarra
                     FROM jugadas J
                     INNER JOIN tipojugadas T ON J.idtipojugada_fk = T.idtipojugadas
                     INNER JOIN loterias L ON J.idloteria_fk  = L.idloteria
-                    INNER JOIN tickets T ON J.idticket_fk = T.idticket";
+                    INNER JOIN tickets T ON J.idticket_fk = T.idticket
+                    INNER JOIN monedas M ON T.monedas_fk = M.idmonedas
+                    INNER JOIN terceros TER ON T.idterceros_fk = TER.idterceros";
 
                     return $conexion->query($sql);
 
