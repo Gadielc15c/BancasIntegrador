0662e5064a10315c92825a150d9fac03220de195
MarielaPM02 <114320802+MarielaPM02@users.noreply.github.com>
2022-11-21 01:25:19 -0400

    cambios

--- frontend/InicioForm.php
+++ frontend/InicioForm.php
@@ -22,6 +22,7 @@ header('location: ../index.php');
   <meta http-equiv="content-type" content="text/html; charset=iso-8859-1" />
   <meta name="description" />
   <meta name="generator" content="HAPedit 3.1" />
+  <meta name="viewport" content="width=device-width, height=device-height, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimun-scale=1.0">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css"
     integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
   <link rel="stylesheet" href="/css/cuerpoWeb.css" />
@@ -42,7 +43,7 @@ header('location: ../index.php');
        
     <div class="" style="margin-left: 40%;">.
                                 <div class="row">
-                                    <div class="col-lg-8  rows">
+                                    <div class="col-lg-8  rows" style="text-align:center">
                                         <h2 class="font-weight-bold" style=" padding-top: 55px; padding-left: 25px">
                                             SECCION DE NOVEDADES
                                         </h2>
@@ -76,8 +77,8 @@ header('location: ../index.php');
        
     <div class="" style="margin-left: 40%;">.
                                 <div class="row">
-                                    <div class="col-lg-8  rows">
-                                        <h2 class="font-weight-bold" style="text-align:center">
+                                    <div class="col-lg-8  rows" style="text-align:center">
+                                        <h2 class="font-weight-bold">
                                             SECCION DE GANADORES
                                         </h2>
                                        
@@ -141,7 +142,7 @@ header('location: ../index.php');
        
     <div class="" style="margin-left: 40%;">.
                                 <div class="row">
-                                    <div class="col-lg-8  rows">
+                                    <div class="col-lg-8  rows"style="text-align:center">
                                         <h2 class="font-weight-bold" style=" padding-top: 55px; padding-left: 25px">
                                             NOSOTROS
                                         </h2>
