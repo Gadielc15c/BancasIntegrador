f816682ed252bf75a149264a6a7d6c4a7bdedd43
MarielaPM02 <114320802+MarielaPM02@users.noreply.github.com>
2022-11-21 21:45:27 -0400

    prueba

--- frontend/mantenimientoTicket.php
+++ frontend/mantenimientoTicket.php
@@ -22,7 +22,6 @@ header('location: ../index.php');
     <meta http-equiv="content-type" content="text/html; charset=iso-8859-1" />
     <meta name="description" />
     <meta name="generator" content="HAPedit 3.1" />
-    <meta name="viewport" content="width=device-width,height=device-height, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimun-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css"
         integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link rel="stylesheet" href="/css/cuerpoWeb.css" />
