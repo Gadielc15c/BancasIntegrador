bbe32a1f74603456edc559c7b555c4685e42dfd9
MarielaPM02 <114320802+MarielaPM02@users.noreply.github.com>
2022-11-22 17:36:22 -0400

    cambio codigo mal

--- js/ticketValidate.js
+++ js/ticketValidate.js
@@ -14,8 +14,6 @@ formJ.addEventListener("submit", e => {
     if(!regexJugada.test(numj.value)){
         warnings +='Uno de los numeros a jugar no es valido <br>'
         entrar=true
-    }else{
-        <input type="submit" class="bebecitoButton" value="Jugar"> </input>
     }
     if(entrar){
         parrafo.innerHTML = warnings
