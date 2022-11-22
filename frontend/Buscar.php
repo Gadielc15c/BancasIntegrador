c1b46c766a3d794f066738d2a58ce339bd6f9b27
Crisropher9807 <grullonperezc@gmail.com>
2022-11-22 02:20:18 -0400

    diseño

---
+++ frontend/Buscar.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>RESUMEN</title>
+    <meta http-equiv="content-type" content="text/html; charset=iso-8859-1" />
+    <meta name="description" />
+    <meta name="generator" content="HAPedit 3.1" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css"
+        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="/css/cuerpoWeb.css" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    
+
+</head>
+<header>
+    <?php include('nav.php');?>
+</header>
+
+<body>
+    
+<div class="container2">
+    <div class="cuadrado">
+  
+
+
+      <div class="container">
+        <div class="box">
+        <div class="iconbox"><i class="fa fa-bar-chart" aria-hidden="true"></i></div>
+        <div class="content">
+            <h3>TOTAL DE TICKETS</h3>
+            <p>contenido...</p>
+        </div>
+
+        </div>
+                   
+        <div class="box">
+        <div class="iconbox"><i class="fa fa-money" aria-hidden="true"></i></div>
+        <div class="content">
+            <h3>TOTAL GANANCIAS</h3>
+            <p>contenido...</p>
+        </div>
+
+        </div>
+                  
+        <div class="box">
+        <div class="iconbox"><i class="fa fa-braille" aria-hidden="true"></i></div>
+        <div class="content">
+            <h3>NUMEROS POPULARES</h3>
+            <p>contenido...</p>
+        </div>
+
+        </div>
+                   
+        <div class="box">
+        <div class="iconbox"><i class="fa fa-sticky-note" aria-hidden="true"></i></div>
+        <div class="content">
+            <h3>LOTERIA POPULAR</h3>
+            <p>contenido...</p>
+        </div>
+
+        </div>
+        
+      </div>
+
+  
+
+
+     </div>
+    </div>
+
+
+
+    <!--No borrar-->
+    <script type="module" src="../js/tarjeta.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <!--Script de iconos-->
+    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+    <!--No borrar-->
+
+</body>
+
+<footer>
+<?php include('../frontend/footer.php');?>
+</footer>
+
+</html>
