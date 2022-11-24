04a5f09b13a26a36b582033a0ac6c6d512a666a6
Crisropher9807 <grullonperezc@gmail.com>
2022-11-24 00:48:29 -0400

    ajustescliente, metodopagocliente, formregsitro en proceso

---
+++ frontend/Formregistro.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>REGISTRO</title>
+    <meta http-equiv="content-type" content="text/html; charset=iso-8859-1" />
+    <meta name="description" />
+    <meta name="generator" content="HAPedit 3.1" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css"
+        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="/css/cuerpoWeb.css" />
+</head>
+
+<header>
+<?php include('../cliente/navCliente.php');?>
+</header>
+
+<body>
+
+    
+
+  <!--No borrar-->
+  <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+  <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+  <!--Script de iconos-->
+  <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+  <!--No borrar-->
+
+</body>
+
+<footer>
+  <?php include('../frontend/footer.php') ?>
+</footer>
+</html>
