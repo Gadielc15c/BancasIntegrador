4166c97e6eeb1af4f0d3aff09cbf84166580c3c2
Crisropher9807 <grullonperezc@gmail.com>
2022-11-24 02:00:14 -0400

    registro

--- frontend/Formregistro.php
+++ frontend/Formregistro.php
@@ -10,6 +10,7 @@
         integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="/css/cuerpoWeb.css" />
+    <link rel="stylesheet" href="/css/login.css" />
 </head>
 
 <header>
@@ -18,8 +19,52 @@
 
 <body>
 
-    
+    <div class="wrapper fadeInDown">
+      <div id="formContent">
 
+      <h2 class="active"> REGISTRO </h2>
+
+      <form>
+  <div class="form-row">
+    <div class="col">
+      <label for=""><span style="color:rgba(245,167,37,1);">Nombre</span></label>
+      <input type="text" class="form-control" placeholder="Ivan">
+    </div>
+    <div class="col">
+      <label for=""><span style="color:rgba(245,167,37,1);">Apellido</span></label>
+      <input type="text" class="form-control" placeholder="Mendoza">
+    </div>
+  </div>
+  
+  <div class="form-row">
+    <div class="col">
+    <label for=""><span style="color:rgba(245,167,37,1);">Direcion</span></label>
+      <input type="text" class="form-control" placeholder="klk@gmail.com">
+    </div>
+    <div class="col">
+    <label for=""><span style="color:rgba(245,167,37,1);">Contraseña</span></label>
+      <input type="text" class="form-control" placeholder="1234">
+    </div>
+  </div>
+
+  <div class="form-row">
+    <div class="col">
+    <label for=""><span style="color:rgba(245,167,37,1);">Direccion</span></label>
+      <input type="text" class="form-control" placeholder="Av. KFC">
+    </div>
+    <div class="col">
+    <label for=""><span style="color:rgba(245,167,37,1);">Telefono</span></label>
+      <input type="text" class="form-control" placeholder="555-555-5555">
+    </div>
+  </div>
+
+
+  <input type="submit" class="fadeIn fourth" value="REGISTRAR" name="login-btn">
+</form>
+
+      </div>
+    </div>
+   
   <!--No borrar-->
   <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
   <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
