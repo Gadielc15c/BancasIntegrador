cece55d76885c31261bdd89a2036953438ebe4d6
MarielaPM02 <114320802+MarielaPM02@users.noreply.github.com>
2022-11-26 01:51:03 -0400

    cambios codigo validacion

--- js/validacionesTicket.js
+++ js/validacionesTicket.js
@@ -1,20 +1,33 @@
 
-document.getElementById("formJ").addEventListener('submit', Validation)
+var formulario = document.getElementById("fomJ");
+var mensaje = document.getElementById("warnings");
+var elementos=document.querySelectorAll("#numj");
 
+
+formulario.addEventListener("submit", Validation)
 
 function Validation(evt){
-    var elementos=document.getElementsByName("jugada[]")
     let regex= /^\d{1,2}$/;
+    let warnings ="";
+    let entrar=false;
+    mensaje.innerHTML =""
 
-    for (i=0; i<elementos.length-1; i++){
-        if(!regex.test(jugada[i].value)){
-
+    elementos.forEach((e) =>{
+        if(!regex.test(e.value) ){
             evt.preventDefault();
+            warnings += 'los campos solo aceptan numeros'
+            entrar= true;
         }
-
-    }
+        if(entrar){
+            mensaje.innerHTML=warnings
+        }
+    
+      });
+    
+    
     
 }
+
 
 
 
